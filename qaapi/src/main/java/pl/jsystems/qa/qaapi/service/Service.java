f66b9fd8b6aeaac892e21238af47c5cf522ab0c8
pawel.dubaj <pdubaj@interia.pl>
2020-11-19 14:06:41 +0100

    user service

--- qaapi/src/main/java/pl/jsystems/qa/qaapi/service/Service.java
+++ qaapi/src/main/java/pl/jsystems/qa/qaapi/service/Service.java
@@ -3,8 +3,10 @@ package pl.jsystems.qa.qaapi.service;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import pl.jsystems.qa.qaapi.specification.Specification;
 
 public class Service {
+
 
     public static Response returnResponse(String host) {
         return RestAssured
@@ -16,5 +18,6 @@ public class Service {
     public static JsonPath returnResponseUnPack(String host) {
         return returnResponse(host).then().extract().body().jsonPath();
     }
+
     
 }
--- qaapi/src/main/java/pl/jsystems/qa/qaapi/service/UserService.java
+++ qaapi/src/main/java/pl/jsystems/qa/qaapi/service/UserService.java
@@ -1,20 +1,37 @@
 package pl.jsystems.qa.qaapi.service;
 
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import pl.jsystems.qa.qaapi.User;
+import pl.jsystems.qa.qaapi.specification.Specification;
 
 import java.util.List;
 
 public class UserService extends Service {
 
-    private static final String USER_HOST = "http://www.mocky.io/v2/5a6a58222e0000d0377a7789";
+    private static final String USERS_PATH = "/5a6a58222e0000d0377a7789";
 
     public static Response returnUserResponse() {
-        return returnResponse(USER_HOST);
+        return returnResponseGetV2();
     }
 
     public static List<User> getUsers() {
-        return returnResponseUnPack(USER_HOST).getList("", User.class);
+//        return returnResponseUnPack(USERS_PATH).getList("", User.class);
+        return returnResponseGetV2UnPack().getList("", User.class);
     }
+
+    private static JsonPath returnResponseGetV2UnPack() {
+        return returnResponseGetV2().then().extract().body().jsonPath();
+    }
+
+    private static Response returnResponseGetV2() {
+        return RestAssured
+                .given()
+                .spec(Specification.requestSpecificationV2())
+                .get(USERS_PATH)
+                .andReturn();
+    }
+
 
 }
